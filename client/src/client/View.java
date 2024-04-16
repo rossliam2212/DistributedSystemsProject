92cc34fd9bd59070373234140a28fa320d2dfee7
rossliam2212@gmail.com <rossliam2212@gmail.com>
2024-04-16 20:43:48 +0100

    Initial commit.

---
+++ client/src/client/View.java
@@ -0,0 +1,24 @@
+package client;
+
+import javax.swing.*;
+
+public class View {
+	// Frames
+	private JFrame frame;
+	
+	// Panels
+	
+	// Labels
+	
+	// Text Fields
+	
+	// Buttons
+	JButton getButton;
+	JButton deleteButton;
+	JButton putButton;
+	JButton postButton;
+	
+	public View() {
+		
+	}
+}
